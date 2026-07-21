d3a143df3f8e1d8e3db86ad8d642c9e62e0dd06d
Soham Phatak <sohamphatakssp@gmail.com>
2026-07-21 14:23:53 +0530

    Fix Internship Fee Amount

--- app/internships/config.ts
+++ app/internships/config.ts
@@ -1,3 +1,3 @@
-export const INTERNSHIP_FEE = 1; // Change to 2500 for production
+export const INTERNSHIP_FEE = 2500; // Change to 2500 for production
 export const INTERNSHIP_FEE_STR = `₹${INTERNSHIP_FEE.toLocaleString('en-IN')}`;
 export const INTERNSHIP_FEE_PAYU = `${INTERNSHIP_FEE}.00`;
