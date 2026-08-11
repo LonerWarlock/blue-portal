5fb9ad2360ab08d712676a82c9e8a5d37e80286c
Soham Phatak <sohamphatakssp@gmail.com>
2026-08-11 21:16:38 +0530

    DDD

--- app/courses/config.ts
+++ app/courses/config.ts
@@ -1,3 +1,3 @@
-export const COURSE_FEE = 2000;
+export const COURSE_FEE = 2000; //REG. COST
 export const COURSE_FEE_STR = `\u20B9${COURSE_FEE.toLocaleString('en-IN')}`;
 export const COURSE_FEE_PAYU = `${COURSE_FEE}.00`;
